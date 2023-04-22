baa69709c6dcea1bf3dc7981d8f8f078d2c7452d
Pawan Verma <pawanzofficial@gmail.com>
2023-04-22 14:37:56 +0530

    server

--- keep-backend/index.js
+++ keep-backend/index.js
@@ -9,7 +9,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const credentials = JSON.parse(fs.readFileSync("./credentials.json"));
+const credentials = JSON.parse(fs.readFileSync("credentials.json"));
 
 admin.initializeApp({
 	credential: admin.credential.cert(credentials),
