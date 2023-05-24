edfad14376f3a3df7cfdba5d1a3c99ed1f76a252
Pawan Verma <pawanzofficial@gmail.com>
2023-05-25 00:31:46 +0530

    final commit

--- keep-backend/index.js
+++ keep-backend/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { db, connectionToDB } from "./db.js";
-import fs from "fs";
 import admin from "firebase-admin";
 import path from "path";
 import "dotenv/config";
@@ -10,9 +9,20 @@ import cors from "cors";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const credentials = JSON.parse(
-	fs.readFileSync(path.join(__dirname, "credentials.json"))
-);
+const credentials = {
+	type: process.env.FIREBASE_TYPE,
+	project_id: process.env.FIREBASE_PROJECT_ID,
+	private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+	private_key: process.env.FIREBASE_PRIVATE_KEY,
+	client_email: process.env.FIREBASE_CLIENT_EMAIL,
+	client_id: process.env.FIREBASE_CLIENT_ID,
+	auth_uri: process.env.FIREBASE_AUTH_URI,
+	token_uri: process.env.FIREBASE_TOKEN_URI,
+	auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+	client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
+
+console.log(credentials);
 
 admin.initializeApp({
 	credential: admin.credential.cert(credentials),
